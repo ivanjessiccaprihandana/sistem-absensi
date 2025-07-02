9c9e4256231d24cb645fb373b0cb3a5de7d50087
ivanjessiccaprihandana <ivandhany17@gmail.com>
2025-07-03 00:27:02 +0700

    Add Meetings model and migration for meeting management

---
+++ app/Models/Meetings.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meetings extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'subject_id',
+        'class_id',
+        'meeting_date',
+        'meeting_number'
+    ];
+        // Meeting.php
+    public function subject() {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function class() {
+        return $this->belongsTo(Classes::class);
+    }
+
+    public function attendances() {
+        return $this->hasMany(Attendances::class, 'meeting_id'); // ✅ sebutkan foreign key yang BENAR
+    }
+    
+
+}
