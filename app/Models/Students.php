f4773f33c1850532305f287f8ce6a6f901990ab8
ivanjessiccaprihandana <ivandhany17@gmail.com>
2025-07-03 00:20:52 +0700

    Refactor Students model to include relationships and fillable attributes

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Students extends Model
 {
-    //
+     use HasFactory;
+    protected $fillable = ['name','class_id','nis'];
+    // Student.php
+    public function class() {
+        return $this->belongsTo(Classes::class); // Gunakan nama Class → Classroom karena "class" reserved word
+    }
+
+    public function attendances() {
+        return $this->hasMany(Attendances::class);
+    }
 }
